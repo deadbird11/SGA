12c62bfaf77f84e2b2eb23302a5ae438bec59303
deadbird11 <eddiejr@hatfieldfamily.com>
2019-12-11 12:11:45 -0500

    moved Blueprint and Gene types into separate header file.

---
+++ SGA/src/Types.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <vector>
+
+namespace sga
+{
+	using BoolGene = std::vector<bool>;
+	using UintGene = std::vector<unsigned int>;
+	using IntGene = std::vector<int>;
+	using FloatGene = std::vector<float>;
+
+	struct GenotypeBlueprint
+	{
+		unsigned int BoolGeneCount = 0;
+		unsigned int UintGeneCount = 0;
+		unsigned int IntGeneCount = 0;
+		unsigned int FloatGeneCount = 0;
+	};
+}
