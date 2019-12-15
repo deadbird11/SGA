774ba80e2178158b63d66332793aa376b312d2dd
deadbird11 <eddiejr@hatfieldfamily.com>
2019-12-15 01:20:02 -0500

    templated the GetRandomInRange function to int and float

--- SGA/src/detail/Random.h
+++ SGA/src/detail/Random.h
@@ -9,10 +9,30 @@ namespace sga
 		// I want to expand on this eventually
 		class Random
 		{
+		friend class Simulation;
 		public:
 		private:
 			static std::default_random_engine s_Engine;
-			static float GetRandomInRange(float a, float b);
+
+			template<typename T>
+			static T GetRandomInRange(T a, T b)
+			{
+				static_assert(false, "detail::Random does not support this data type for ranges!");
+			}
+
+			template<>
+			static float GetRandomInRange(float a, float b)
+			{
+				std::uniform_real_distribution<float> distribution(a, b);
+				return distribution(s_Engine);
+			}
+
+			template<>
+			static int GetRandomInRange(int a, int b)
+			{
+				std::uniform_int_distribution<int> distribution(a, b);
+				return distribution(s_Engine);
+			}
 		};
 	}
 }
